c0a1edba5a4d5b128f46014d7340e3a0c7d4a5ce
Raphaël Rougeron <goldoraf@gmail.com>
2015-09-24 15:59:36 +0200

    Fixed Gruntfile so that watch triggers uglify too

--- Gruntfile.js
+++ Gruntfile.js
@@ -57,7 +57,7 @@ module.exports = function (grunt) {
     watch: {
       runtime: {
         files: ['src/runtime/*.js', 'src/mixins/*.js'],
-        tasks: ['concat:runtime']
+        tasks: ['build']
       }
     }
 

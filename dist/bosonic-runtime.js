2cd6b167662d5a54e1f616313f4e41c08a72da4c
Raphaël Rougeron <goldoraf@gmail.com>
2015-09-24 15:58:59 +0200

    Added basic support for key events listeners setup

--- dist/bosonic-runtime.js
+++ dist/bosonic-runtime.js
@@ -1021,18 +1021,49 @@ Bosonic.CustomAttributes = {
         return this._hasPrefixedAttribute(name) ? 'data-' + name : name;
     }
 };
+var KEYS = {
+    9: 'tab',
+    13: 'enter',
+    27: 'esc',
+    33: 'pageup',
+    34: 'pagedown',
+    35: 'end',
+    36: 'home',
+    32: 'space',
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down',
+    46: 'del',
+    106: '*'
+};
+
+var MODIFIER_KEYS = {
+    'shift': 'shiftKey',
+    'ctrl': 'ctrlKey',
+    'alt': 'altKey',
+    'meta': 'metaKey'
+};
+
 Bosonic.Events = {
     __boundHandlers: {},
+    __boundKeyListener: null,
 
     created: function() {
         for (var eventName in this.listeners) {
             this.listen(this, eventName, this.listeners[eventName]);
+        }
+        if (this.keyBindings) {
+            this._setupKeyListener();
         }
     },
 
     detached: function() {
         for (var eventName in this.listeners) {
             this.unlisten(this, eventName, this.listeners[eventName]);
+        }
+        if (this.keyBindings) {
+            this._removeKeyListener();
         }
     },
 
@@ -1059,6 +1090,26 @@ Bosonic.Events = {
 
     _getHandlerKey: function(eventName, methodName) {
         return eventName + ':' + methodName;
+    },
+
+    _setupKeyListener: function() {
+        this.__boundKeyListener = this._keyListener.bind(this);
+        this.addEventListener('keydown', this.__boundKeyListener);
+    },
+
+    _removeKeyListener: function() {
+        this.removeEventListener('keydown', this.__boundKeyListener);
+    },
+
+    _keyListener: function(event) {
+        for (var k in this.keyBindings) {
+            keys = k.split(' ');
+            var handlerName = this.keyBindings[k];
+            var pressedKey = KEYS[event.keyCode] ? KEYS[event.keyCode] : event.keyCode;
+            if (!event.defaultPrevented && keys.indexOf(pressedKey) !== -1) {
+                this[handlerName].call(this, event);
+            }
+        }
     }
 };
 Bosonic.Selection = {
--- src/mixins/events.js
+++ src/mixins/events.js
@@ -1,15 +1,46 @@
+var KEYS = {
+    9: 'tab',
+    13: 'enter',
+    27: 'esc',
+    33: 'pageup',
+    34: 'pagedown',
+    35: 'end',
+    36: 'home',
+    32: 'space',
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down',
+    46: 'del',
+    106: '*'
+};
+
+var MODIFIER_KEYS = {
+    'shift': 'shiftKey',
+    'ctrl': 'ctrlKey',
+    'alt': 'altKey',
+    'meta': 'metaKey'
+};
+
 Bosonic.Events = {
     __boundHandlers: {},
+    __boundKeyListener: null,
 
     created: function() {
         for (var eventName in this.listeners) {
             this.listen(this, eventName, this.listeners[eventName]);
+        }
+        if (this.keyBindings) {
+            this._setupKeyListener();
         }
     },
 
     detached: function() {
         for (var eventName in this.listeners) {
             this.unlisten(this, eventName, this.listeners[eventName]);
+        }
+        if (this.keyBindings) {
+            this._removeKeyListener();
         }
     },
 
@@ -36,5 +67,25 @@ Bosonic.Events = {
 
     _getHandlerKey: function(eventName, methodName) {
         return eventName + ':' + methodName;
+    },
+
+    _setupKeyListener: function() {
+        this.__boundKeyListener = this._keyListener.bind(this);
+        this.addEventListener('keydown', this.__boundKeyListener);
+    },
+
+    _removeKeyListener: function() {
+        this.removeEventListener('keydown', this.__boundKeyListener);
+    },
+
+    _keyListener: function(event) {
+        for (var k in this.keyBindings) {
+            keys = k.split(' ');
+            var handlerName = this.keyBindings[k];
+            var pressedKey = KEYS[event.keyCode] ? KEYS[event.keyCode] : event.keyCode;
+            if (!event.defaultPrevented && keys.indexOf(pressedKey) !== -1) {
+                this[handlerName].call(this, event);
+            }
+        }
     }
 };

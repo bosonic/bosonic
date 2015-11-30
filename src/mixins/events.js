2c566dbc3fb688d4e2dc2c653c81b45ba74e83cd
Raphaël Rougeron <goldoraf@gmail.com>
2015-11-30 17:46:05 +0100

    Rewrote events mixin for better maintenability & extensibility

--- src/mixins/events.js
+++ src/mixins/events.js
@@ -1,63 +1,6 @@
-var GESTURE_FLAG = '__bosonicGestures';
-
-var TRACK_DISTANCE = 10;
-
-var GESTURES = {
-    tap: {
-        condition: function(state, event) {
-            return event.type === 'pointerup' &&
-                Math.abs(state.startX - event.clientX) < TRACK_DISTANCE &&
-                Math.abs(state.startY - event.clientY) < TRACK_DISTANCE;
-        }
-    },
-
-    hold: {
-        setup: function(state) {
-            var that = this;
-            state.__timer = setTimeout(function() {
-                that.fireCustomEvent('hold', state);
-                that._removeDocumentListeners(state);
-            }, 1000);
-        },
-        condition: function(state, event) {
-            if (Math.abs(state.startX - event.clientX) >= TRACK_DISTANCE
-                || Math.abs(state.startY - event.clientY) >= TRACK_DISTANCE) {
-                clearTimeout(state.__timer);
-            }
-        },
-        teardown: function(state) {
-            clearTimeout(state.__timer);
-        }
-    },
-
-    track: {
-        condition: function(state, event) {
-            var hasMovedEnough = Math.abs(state.startX - event.clientX) >= TRACK_DISTANCE
-                              || Math.abs(state.startY - event.clientY) >= TRACK_DISTANCE;
-            if (!hasMovedEnough) return;
-            
-            if (event.type === 'pointermove') {
-                var x = event.clientX,
-                    y = event.clientY;
-                state.dx = x - state.startX;
-                state.dy = y - state.startY;
-                state.ddx = state.lastX ? x - state.lastX : 0;
-                state.ddy = state.lastY ? y - state.lastY : 0;
-                state.lastX = x;
-                state.lastY = y;
-                this.fireCustomEvent('track', state, event);
-            } else if (event.type === 'pointerup') return true;
-        }
-    }
-};
-
-function isGestureEvent(eventName) {
-    return GESTURES.hasOwnProperty(eventName);
-}
+var TAP_TRESHOLD = 10;
 
 Bosonic.Events = {
-    __boundHandlers: {},
-
     created: function() {
         for (var eventName in this.listeners) {
             this.listen(this, eventName, this.listeners[eventName]);
@@ -83,22 +26,92 @@ Bosonic.Events = {
         return event;
     },
 
-    listen: function(node, eventName, methodName, useCapture) {
-        if (isGestureEvent(eventName)) {
-            this._listenToGesture(node, eventName, methodName);
-        } else {
-            if (useCapture !== true) useCapture = false;
-            node.addEventListener(eventName, this._registerHandler(eventName, methodName), useCapture);
+    listen: function(node, eventName, methodName) {
+        var listener = this._findListener(this, node, eventName, methodName);
+        if (!listener) {
+            listener = this._registerListener(this, node, eventName, methodName);
+        }
+        if (listener.deps) {
+            Object.keys(listener.deps).forEach(function(dep) {
+                this._listen(node, dep, listener.deps[dep]);
+            }, this);
+        }
+        this._listen(node, eventName, listener.handler);
+    },
+
+    unlisten: function(node, eventName, methodName) {
+        var listener = this._findListener(this, node, eventName, methodName);
+        if (listener) {
+            if (listener.deps) {
+                Object.keys(listener.deps).forEach(function(dep) {
+                    this._unlisten(node, dep, listener.deps[dep]);
+                }, this);
+            }
+            this._unlisten(node, eventName, listener.handler);
         }
     },
 
-    unlisten: function(node, eventName, methodName, useCapture) {
-        if (isGestureEvent(eventName) && node[GESTURE_FLAG] === true && this.__pointerdownHandler) {
-            node.removeEventListener('pointerdown', this.__pointerdownHandler);
-            delete node[GESTURE_FLAG];
+    _listen: function(node, eventName, handler) {
+        node.addEventListener(eventName, handler);
+    },
+
+    _unlisten: function(node, eventName, handler) {
+        node.removeEventListener(eventName, handler);
+    },
+
+    _findListener: function(host, node, eventName, methodName) {
+        return this._getListener(host, node, this._getListenerKey(eventName, methodName));
+    },
+
+    _getListener: function(host, node, key) {
+        if (!host.__listeners || !host.__listeners[key]) return;
+        var listeners = host.__listeners[key],
+            len = listeners.length;
+        if (len === 1) return listeners[0];
+        for (var i = 0; i < len; i++) {
+            if (listeners[i].node === node) return listeners[i];
         }
-        if (useCapture !== true) useCapture = false;
-        node.removeEventListener(eventName, this._getHandler(eventName, methodName), useCapture);
+    },
+
+    _registerListener: function(host, node, eventName, methodName) {
+        var key = this._getListenerKey(eventName, methodName),
+            listener = this._prepareListener(host, node, eventName, methodName);
+        
+        this._setListener(host, node, key, listener);
+        return listener;
+    },
+
+    _prepareListener: function(host, node, eventName, methodName) {
+        if (!host[methodName]) {
+            throw 'Event handler method `' + methodName + '` is not defined';
+        }
+        var listener = {
+            node: node,
+            handler: function(e) {
+                host[methodName](e);
+            }
+        };
+        if (this.customEvents[eventName]) {
+            var detail = {},
+                customEvent = this.customEvents[eventName];
+
+            listener.deps = {};
+            customEvent.deps.forEach(function(dep) {
+                var handler = customEvent[dep].bind(this, detail);
+                listener.deps[dep] = handler;
+            });
+        }
+        return listener;
+    },
+
+    _setListener: function(host, node, key, listener) {
+        if (!host.__listeners) host.__listeners = {};
+        if (!host.__listeners[key]) host.__listeners[key] = [];
+        host.__listeners[key].push(listener);
+    },
+
+    _getListenerKey: function(eventName, methodName) {
+        return eventName + ':' + methodName;
     },
 
     trackPointer: function(initialEvent, callbackName, stopCallbackName) {
@@ -142,99 +155,18 @@ Bosonic.Events = {
         }
     },
 
-    _registerHandler: function(eventName, methodName) {
-        if (!this[methodName]) {
-            throw 'Event handler method `' + methodName + '` is not defined';
-        }
-        var handler = this[methodName].bind(this);
-        this.__boundHandlers[this._getHandlerKey(eventName, methodName)] = handler;
-        return handler;
-    },
-
-    _getHandler: function(eventName, methodName) {
-        return this.__boundHandlers[this._getHandlerKey(eventName, methodName)];
-    },
-
-    _getHandlerKey: function(eventName, methodName) {
-        return eventName + ':' + methodName;
-    },
-
-    // GESTURES
-    __documentEventsHandler: null,
-    __pointerdownHandler: null,
-
-    fireCustomEvent: function(gesture, state, originalEvent) {
-        if (originalEvent) {
-            state.originalEvent = originalEvent;
-        }
-        var ev = this.fire(gesture, state, {
-            bubbles: true,
-            cancelable: true,
-            node: state.target
-        });
-        // preventDefault() forwarding
-        if (ev.defaultPrevented) {
-            if (originalEvent && originalEvent.preventDefault) {
-                originalEvent.preventDefault();
-                originalEvent.stopImmediatePropagation();
+    customEvents: {
+        tap: {
+            deps: ['pointerdown', 'pointerup'],
+            pointerdown: function(detail, e) {
+                detail.x = e.clientX;
+                detail.y = e.clientY;
+            },
+            pointerup: function(detail, e) {
+                if (e.button === 0 &&
+                   Math.abs(detail.x - e.clientX) < TAP_TRESHOLD &&
+                   Math.abs(detail.y - e.clientY) < TAP_TRESHOLD) Bosonic.Events.fire('tap', detail, { node: e.currentTarget });
             }
         }
-    },
-
-    _listenToGesture: function(node, eventName, methodName) {
-        if (node[GESTURE_FLAG] === undefined) {
-            if (!this.__pointerdownHandler) {
-                this.__pointerdownHandler = this._handlePointerdown.bind(this);
-            }
-            node.addEventListener('pointerdown', this.__pointerdownHandler);
-            node[GESTURE_FLAG] = true;
-        }
-
-        node.addEventListener(eventName, this._registerHandler(eventName, methodName));
-    },
-
-    _handlePointerdown: function(event) {
-        var state = {
-            target: event.target,
-            startX: event.clientX,
-            startY: event.clientY
-        };
-
-        Object.keys(GESTURES).forEach(function(gesture) {
-            var setup = GESTURES[gesture].setup;
-            if (setup) {
-                setup.call(this, state);
-            }
-        }, this);
-        
-        this._addDocumentListeners(state);
-    },
-
-    _handleDocumentEvent: function(state, event) {
-        for (var gesture in GESTURES) {
-            var condition = GESTURES[gesture].condition;
-            if (condition && condition.call(this, state, event) === true) {
-                this.fireCustomEvent(gesture, state, event);
-                this._removeDocumentListeners(state);
-                break;
-            }
-        }
-    },
-
-    _addDocumentListeners: function(state) {
-        this.__documentEventsHandler = this._handleDocumentEvent.bind(this, state);
-        document.addEventListener('pointermove', this.__documentEventsHandler);
-        document.addEventListener('pointerup', this.__documentEventsHandler);
-    },
-
-    _removeDocumentListeners: function(state) {
-        Object.keys(GESTURES).forEach(function(gesture) {
-            var teardown = GESTURES[gesture].teardown;
-            if (teardown) {
-                teardown.call(this, state);
-            }
-        }, this);
-        document.removeEventListener('pointermove', this.__documentEventsHandler);
-        document.removeEventListener('pointerup', this.__documentEventsHandler);
     }
 };

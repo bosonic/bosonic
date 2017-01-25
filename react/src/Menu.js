5a8842960de726060762cfc078251f00d5d3fdfd
Raphaël Rougeron <goldoraf@gmail.com>
2017-01-25 13:11:35 +0100

    Export Item from Menu

--- react/src/Menu.js
+++ react/src/Menu.js
@@ -2,9 +2,11 @@ import React from 'react'
 import reactify from '../vendor/react-integration'
 
 import MenuButtonElement from '../../elements/lib/b-menu-button'
-import Listbox from './Listbox'
+import Listbox, { Item } from './Listbox'
 
 export const MenuButton = reactify(MenuButtonElement)
 const Menu = Listbox
+
+export { Item }
 
 export default Menu
